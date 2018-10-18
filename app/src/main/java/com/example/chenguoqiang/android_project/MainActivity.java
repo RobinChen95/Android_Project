d588647e6bc95acefa8cf7fd256894f02c75c0a2
chenguoqiang <407339708@qq.com>
2018-10-18 10:29:08 +0800

    add Parser

--- app/src/main/java/com/example/chenguoqiang/android_project/MainActivity.java
+++ app/src/main/java/com/example/chenguoqiang/android_project/MainActivity.java
@@ -11,15 +11,56 @@ import android.widget.Toast;
 
 import com.pku.edu.ChenGuoqiang.util.NetUtil;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private ImageView mUpdateBtn;
+
+    private void parseXML(String xmldata) {
+        try {
+            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType = xmlPullParser.getEventType();
+            Log.d("myWeather", "parseXML");
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals("city")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "city:" + xmlPullParser.getText());
+                        }
+                        else if (xmlPullParser.getName().equals("updatetime")){
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "updatetime:" + xmlPullParser.getText());
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                            break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private void queryWeatherCode(String cityCode) {
         final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
@@ -44,6 +85,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     }
                     String responseStr = response.toString();
                     Log.d("myWeather", responseStr);
+                    parseXML(responseStr);
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
