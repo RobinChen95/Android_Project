e75dd35d889560018a081ea6e257e4529c17cf91
chenguoqiang <407339708@qq.com>
2018-11-01 21:06:39 +0800

    remake select

--- app/src/main/java/com/example/chenguoqiang/android_project/SelectCity.java
+++ app/src/main/java/com/example/chenguoqiang/android_project/SelectCity.java
@@ -5,11 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.pku.edu.ChenGuoqiang.app.MyApplication;
+import com.pku.edu.ChenGuoqiang.bean.City;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectCity extends Activity implements View.OnClickListener {
 
     private ImageView mBackBtn;
+    private ListView mList;
+    private List<City> cityList;
+    private ArrayAdapter<String> madpter;
+    private ArrayList<String> city_name;
+    private String citycode;
+    private String cityname;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +37,27 @@ public class SelectCity extends Activity implements View.OnClickListener {
 
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+        initViews();
+    }
+
+    private void initViews(){
+        city_name = new ArrayList<>();
+        mList = (ListView)findViewById(R.id.city_list);
+        MyApplication myApplication = (MyApplication) getApplication();
+        cityList = myApplication.getmCityList();
+        for (City city : cityList){
+            city_name.add(city.getCity());
+        }
+        madpter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,city_name);
+        mList.setAdapter(madpter);
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
+                cityname=cityList.get(position).getCity();
+                citycode=cityList.get(position).getNumber();
+                Toast.makeText(SelectCity.this,cityname+":"+citycode,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
