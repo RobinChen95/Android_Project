46454723a3fa8cc7305863ccd3208d0622a170ba
chenguoqiang <407339708@qq.com>
2018-11-05 15:03:18 +0800

    在另外一个Windows设备上面安装了Android Studio作为协同开发

--- app/src/main/java/com/example/chenguoqiang/android_project/MainActivity.java
+++ app/src/main/java/com/example/chenguoqiang/android_project/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.chenguoqiang.android_project;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
