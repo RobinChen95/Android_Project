18536872fdfc5cd0f0b3180093e1d1d943d45896
陈国强 <407339708@qq.com>
2019-04-02 00:52:14 +0800

    ??? update

--- app/src/main/java/com/example/chenguoqiang/android_project/SelectCity.java
+++ app/src/main/java/com/example/chenguoqiang/android_project/SelectCity.java
@@ -78,12 +78,10 @@ public class SelectCity extends Activity implements View.OnClickListener {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
             }
-
             @Override
             public void afterTextChanged(Editable s) {
                 filterData(s);
